c4a0b98c9690fd8e54b8808b799608e9767d6e82
SolbiShin <sbshin.dev@gmail.com>
2021-11-16 00:51:46 +0900

    styled-component 라이브러리 사용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import SassComponent from './SassComponent';
-import CSSModule from './CSSModule';
+// import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
 
-
-function App() {
-  return (
-    <div>
-      <CSSModule />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
