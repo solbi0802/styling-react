2e563cbfd12e0ce8181bce69e3f43255da9c36cc
SolbiShin <sbshin.dev@gmail.com>
2021-11-16 00:12:21 +0900

    css module 사용하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import SassComponent from './SassComponent';
+// import SassComponent from './SassComponent';
+import CSSModule from './CSSModule';
+
 
 function App() {
   return (
     <div>
-      <SassComponent />
+      <CSSModule />
     </div>
   );
 }
